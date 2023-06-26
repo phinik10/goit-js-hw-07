22598bdc283c137ea5d5e01d28d5ff313d53e389
Anastasia <123020355+phinik10@users.noreply.github.com>
2023-06-26 14:58:10 +0200

    Update 01-gallery.js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,20 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
-
 console.log(galleryItems);
+const galleryRef = document.querySelector('.gallery');
+
+const createGalleryItemMarkup = ({ preview, original, description }) => {
+  return `
+    <li class="gallery__item">
+      <a class="gallery__link" href="${original}">
+        <img class="gallery__image" src="${preview}" alt="${description}" />
+      </a>
+    </li>
+  `;
+};
+
+const galleryItemsMarkup = galleryItems
+  .map(createGalleryItemMarkup)
+  .join('');
+
+galleryRef.innerHTML = galleryItemsMarkup;
