0025c12cd1019c3be19f88567ed98541b0886951
Anastasia <123020355+phinik10@users.noreply.github.com>
2023-07-06 18:23:34 +0200

    Update 02-lightbox.js

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,24 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+const gallery = document.querySelector('.gallery');
+
+const galleryMarkup = galleryItems
+  .map(
+    ({ preview, original, description }) => `
+    <li class="gallery__item">
+      <a class="gallery__link" href="${original}">
+        <img class="gallery__image" src="${preview}" alt="${description}" />
+      </a>
+    </li>
+  `
+  )
+  .join('');
+
+gallery.innerHTML = galleryMarkup;
+
+// Ініціалізація бібліотеки SimpleLightbox
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
